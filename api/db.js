4b1a75e253f527b0a608b344170105e30c53f286
olgaodc <olgaodincova@gmail.com>
2023-12-29 17:03:24 +0200

    Db pool changed

--- api/db.js
+++ api/db.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Pool } =  require('pg');
 const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: true,
   user: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
